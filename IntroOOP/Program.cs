7d618ff5dea14b4005650a067e1e05b298fb040d
Ismoil <ism0il.muhammadiev92@gmail.com>
2022-07-07 20:26:12 +0500

    Первая задача из третьего домашнего задание выполнено

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using IntroOOP.Task1;
+
+/// <summary>
+/// Задача номер 1.
+/// </summary>
+static void Task1()
+{
+    Console.WriteLine("========   Задача номер 1 ======================");
+    Console.WriteLine("=================================================");
+    Console.WriteLine();
+    AccountBank account1 = new() { AccountNumber = 111222333, Balance = 100000, BankTypeAccount = BankTypeAccount.Credit };
+    AccountBank account2 = new() { AccountNumber = 222333111, Balance = 200000, BankTypeAccount = BankTypeAccount.Deposit };
+    Console.Write("Введите сумму для снятия денег в банке на счету:");
+    int money = int.Parse(Console.ReadLine());
+    account1.TransferMoney(ref account2, money);
+}
+
+Task1();
---
+++ IntroOOP/Task1/AccountBank.cs
@@ -0,0 +1,48 @@
+﻿namespace IntroOOP.Task1;
+
+public class AccountBank
+{
+    /// <summary>
+    /// Свойство Номер счёта
+    /// </summary>
+    public int AccountNumber { get; set; }
+
+    /// <summary>
+    /// Свойство Баланс
+    /// </summary>
+    public int Balance { get; set; }
+
+    /// <summary>
+    /// Тип банковского счёта
+    /// </summary>
+    public BankTypeAccount BankTypeAccount { get; set; }
+
+    /// <summary>
+    /// Метод, для перевода деньги с одного счета на другое
+    /// </summary>
+    /// <param name="account">счёт у которого будет сниматься деньги</param>
+    /// <param name="money">сумма которое будет сниматься</param>
+    /// <returns>возвращает true, если баланс больше чем сумма которое будет сниматься, false если баланс меньше чем сумма</returns>
+    public bool TransferMoney(ref AccountBank account, int money)
+    {
+        if (account.Balance >= money)
+        {
+            this.Balance += money;
+            account.Balance -= money;
+            return true;
+        }
+        else
+            return false;
+    }
+}
+
+/// <summary>
+/// Тип Банковского счёта, перечисление
+/// </summary>
+public enum BankTypeAccount
+{
+    Current,
+    Estimated,
+    Credit,
+    Deposit
+}

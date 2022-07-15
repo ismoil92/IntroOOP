057ae053940f6cc2f9659e26332391bc2b628e6e
Ismoil <ism0il.muhammadiev92@gmail.com>
2022-07-15 16:48:24 +0500

    Вторая задача из пятого домашнего задание выполнено

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,14 +1,79 @@
 ﻿using IntroOOP.Task1;
-
-Task1();
-
+using IntroOOP.Task2;
 
 
+
+SelectTasks();
+
+static void SelectTasks()
+{
+    Console.WriteLine("===================== Задача номер 1 =====================");
+    Console.WriteLine("===================== Задача номер 2 =====================");
+    Console.Write("Выберите номер задач:");
+    int numb = int.Parse(Console.ReadLine()!);
+    switch(numb)
+    {
+        case 1:
+            Task1();
+            break;
+        case 2:
+            Task2();
+            break;
+        default:
+            Console.WriteLine("Вы ввели некорректный номер задач");
+            break;
+    }
+}
+
+
+
+/// <summary>
+/// Задача номер 2
+/// </summary>
+static void Task2()
+{
+    Console.WriteLine("====================== Задача номер 2 =====================");
+    Console.WriteLine("===========================================================");
+    Console.WriteLine();
+    ComplexNumber c1 = new(2, 3);
+    ComplexNumber c2 = new(2, 3);
+    Console.Write("Выберите одну из трёх операции (+, -, *):");
+    char operation = char.Parse(Console.ReadLine()!);
+    switch(operation)
+    {
+        case '+':
+           var plus = c1 + c2;
+            break;
+        case '-':
+            var minus = c1 - c2;
+            break;
+        case '*':
+            var multiply = c1 * c2;
+            break;
+        default:
+            Console.WriteLine("Выберите одну из трёх операции: +, -, *");
+            break;
+    }
+
+    ///Сравнение двух комплексных чисел
+    bool istrue1 = c1 == c2;
+    bool istrue2 = c1 != c2;
+    bool istrue3 = c1.Equals(c2);
+}
+
+
+
+/// <summary>
+/// Задача номер 1
+/// </summary>
 static void Task1()
 {
-    Rational r1 = new Rational(3, 4);
-    Rational r2 = new Rational(4, 5);
-    Console.Write("Выберите одну из пяти операции:");
+    Console.WriteLine("====================== Задача номер 1 =====================");
+    Console.WriteLine("===========================================================");
+    Console.WriteLine();
+    Rational r1 = new (3, 4);
+    Rational r2 = new (4, 5);
+    Console.Write("Выберите одну из пяти операции (+, -, *, /, %):");
     char operation = char.Parse(Console.ReadLine()!);
 
     switch (operation)

a6a33ee360e3c4fec6cf3d9e54ed3ef916257721
Ismoil <ism0il.muhammadiev92@gmail.com>
2022-07-15 11:36:44 +0500

    Первая задачи из пятой домашней работы выполнено

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,23 +1,78 @@
 ﻿using IntroOOP.Task1;
 
-Rational r1 = new Rational(3, 4);
-Rational r2 = new Rational(4, 5);
+Task1();
 
 
-if (r1 < r2)
-    Console.WriteLine(r1 < r2);
-else
-    Console.WriteLine(r1 < r2);
-if (r1 > r2)
-    Console.WriteLine(r1 > r2);
-else
-    Console.WriteLine(r1 > r2);
-if (r1 <= r2)
-    Console.WriteLine(r1 <= r2);
-else
-    Console.WriteLine(r1 <= r2);
-if (r1 >= r2)
-    Console.WriteLine(r1 >= r2);
-else
-    Console.WriteLine(r1 >= r2);
-Console.WriteLine("The end!");
+
+static void Task1()
+{
+    Rational r1 = new Rational(3, 4);
+    Rational r2 = new Rational(4, 5);
+    Console.Write("Выберите одну из пяти операции:");
+    char operation = char.Parse(Console.ReadLine()!);
+
+    switch (operation)
+    {
+        case '+':
+            /// сложение
+            var plus1 = r1 + r2;
+            var plus2 = r1 + 3;
+            var plus3 = 3 + r2;
+            break;
+        case '-':
+            /// вычитание 
+            var minus1 = r1 - r2;
+            var minus2 = 3 - r2;
+            var minus3 = r1 - 3;
+            break;
+        case '*':
+            ///умножение
+            var multiply1 = r1 * r2;
+            var multiply2 = 3 * r2;
+            var multiply3 = r1 * 3;
+            break;
+        case '/':
+            ///деление
+            var devided1 = r1 / r2;
+            var devided2 = r1 / 3;
+            var devided3 = 3 / r2;
+            break;
+        case '%':
+            //остаток от деления
+            double procent = r1 % r2;
+            double procent2 = r1 % 0.1;
+            double procent3 = 1.5 % r2;
+            break;
+        default:
+            Console.WriteLine("Нет такого знака!!!");
+            break;
+    }
+
+
+
+    ///Инкремент Декремент
+    r1++;
+    r2++;
+    r1--;
+    r2--;
+
+
+    ///Явная преобразование
+    (int numerator, int denominator) = (((int, int))r1);
+    (float numeratorfloat, float denominatorfloat) = (((float, float))r1);
+
+    (int numeratorint, int denominatorint) = (((int, int))r2);
+    (float numerator_float, float denominator_float) = (((float, float))r2);
+
+
+
+    //Сравнение двух рациональных чисел
+    bool _bool1 = r1 > r2;
+    bool _bool2 = r1 >= r2;
+    bool _bool3 = r1 < r2;
+    bool _bool4 = r1 <= r2;
+    bool _bool5 = r1 == r2;
+    bool _bool6 = r1 != r2;
+    bool _bool7 = r1.Equals(r2);
+}
+
--- IntroOOP/Task1/Rational.cs
+++ IntroOOP/Task1/Rational.cs
@@ -2,8 +2,8 @@
 
 public class Rational
 {
-    private double _numerator; // числитель
-    private double _denominator; // знаменатель
+    private  double _numerator; // числитель
+    private  double _denominator; // знаменатель
 
     public Rational(double numerator, double denominator)
     {
@@ -114,37 +114,142 @@ public class Rational
     /// <param name="r2">второй рациональное число</param>
     /// <returns>возвращает новый рациональное число</returns>
 
-    public static Rational operator *(Rational r1, Rational r2)
-    {
-        return new Rational(r1._numerator * r2._numerator, r1._denominator * r2._denominator);
-    }
+    public static Rational operator *(Rational r1, Rational r2) => new Rational(r1._numerator * r2._numerator, r1._denominator * r2._denominator);
 
 
     /// <summary>
-    /// Перегрузка оператора *, для умножение рационального числа и переменный типо double
+    /// Перегрузка оператора *, для умножение рационального числа и переменный типа double
     /// </summary>
     /// <param name="r1">рациональное число</param>
     /// <param name="numerator">переменный типа double</param>
     /// <returns>возвращает новый рациональное число</returns>
-    public static Rational operator *(Rational r1, double numerator)
-    {
-        return new Rational(r1._numerator * numerator, r1._denominator);
-    }
+    public static Rational operator *(Rational r, double numerator) => new Rational(r._numerator * numerator, r._denominator);
 
 
 
     /// <summary>
-    /// Перегрузка оператора *, для умножение переменный типо double и рационального числа 
+    /// Перегрузка оператора *, для умножение переменный типа double и рационального числа 
     /// </summary>
     /// <param name="numerator">переменный типа double</param>
     /// <param name="r2">рациональное число</param>
     /// <returns>возвращает новый рациональное число</returns>
 
-    public static Rational operator *(double numerator, Rational r2)
+    public static Rational operator *(double numerator, Rational r) => new Rational(numerator * r._numerator, r._denominator);
+
+
+
+    /// <summary>
+    /// Перегрузка оператора /, для деление двух рациональных чисел
+    /// </summary>
+    /// <param name="r1">первое рациональное число</param>
+    /// <param name="r2">второй рациональное число</param>
+    /// <returns>возвращает новый рациональное число</returns>
+    public static Rational operator /(Rational r1, Rational r2) => new Rational(r1._numerator * r2._denominator, r1._denominator * r2._numerator);
+
+
+    /// <summary>
+    /// Перегрузка оператора /, для деление рационального числа и переменный типа double
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <param name="numerator">переменный типа double</param>
+    /// <returns>возвращает новый рациональное число</returns>
+    public static Rational operator /(Rational r, double numerator) => new Rational(r._numerator, r._denominator * numerator);
+
+
+    /// <summary>
+    /// Перегрузка оператора /, для деление переменный типа double и рационального числа 
+    /// </summary>
+    /// <param name="numerator">переменный типа double</param>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает новый рациональное число</returns>
+
+    public static Rational operator /(double numerator, Rational r)
     {
-        return new Rational(numerator * r2._numerator, r2._denominator);
+        return new Rational(numerator * r._denominator, r._numerator);
     }
 
+
+
+
+    /// <summary>
+    /// Перегрузка оператора %, для нахождение остатка от деление  двух рациональных чисел
+    /// </summary>
+    /// <param name="r1">первое рациональное число</param>
+    /// <param name="r2">второе рациональное число</param>
+    /// <returns>возвращает десятичный дробь типа double</returns>
+
+    public static double operator %(Rational r1, Rational r2) => Rational.Fraction(r1) % Rational.Fraction(r2);
+
+
+
+    /// <summary>
+    /// Перегрузка оператора %, для нахождение остатка от деление рационального числа и переменный типа double
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <param name="number">переменный типа double</param>
+    /// <returns>возвращает десятичный дробь типа double</returns>
+    public static double operator %(Rational r, double number) => Rational.Fraction(r) % number;
+
+
+
+
+    /// <summary>
+    /// Перегрузка оператора %, для нахождение остатка от деление переменный типа double и рационального числа
+    /// </summary>
+    /// <param name="number">переменный типа double</param>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает десятичный дробь типа double</returns>
+    public static double operator %(double number, Rational r) => number % Rational.Fraction(r);
+
+
+
+    /// <summary>
+    /// Перегрузка оператора ++ (инкремент), для увелечение на единицу элементы рациональное число
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает новый рациональное число</returns>
+    public static Rational operator ++(Rational r)
+    {
+        r._numerator++;
+        r._denominator++;
+        return r;
+    }
+
+
+    /// <summary>
+    /// Перегрузка оператора -- (декремент), для уменьшение на единицу элементы рациональное число
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает новый рациональное число</returns>
+    public static Rational operator --(Rational r)
+    {
+        r._numerator--;
+        r._denominator--;
+        return r;
+    }
+
+
+
+    /// <summary>
+    /// Явная преобразование элементы типа double рационального числа в типа int
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает кортежи (два элемента) типа int</returns>
+    public static explicit operator (int, int)(Rational r)
+    {
+        return ((int)r._numerator, (int)r._denominator);
+    }
+
+
+    /// <summary>
+    /// Явная преобразование элементы типа double рационального числа в типа float
+    /// </summary>
+    /// <param name="r">рациональное число</param>
+    /// <returns>возвращает кортежи (два элемента) типа float</returns>
+    public static explicit operator (float, float)(Rational r)
+    {
+        return ((float)r._numerator, (float)r._denominator);
+    }
 
     /// <summary>
     /// Перегрузка оператора == для сравнение двух рациональных чисел
@@ -152,10 +257,7 @@ public class Rational
     /// <param name="r1">первый рациональное число</param>
     /// <param name="r2">второй рациональное число</param>
     /// <returns>возвращает true, если они равны, false, если не равны</returns>
-    public static bool operator == (Rational r1, Rational r2)
-    {
-        return Rational.Fraction(r1)==Rational.Fraction(r2);
-    }
+    public static bool operator == (Rational r1, Rational r2)=> Equals(r1, r2);
 
 
 
@@ -165,10 +267,7 @@ public class Rational
     /// <param name="r1">первый рациональное число</param>
     /// <param name="r2">второй рациональное число</param>
     /// <returns>возвращает true, если они не равны, false, если равны</returns>
-    public static bool operator != (Rational r1, Rational r2)
-    {
-        return Rational.Fraction(r1) != Rational.Fraction(r2);
-    }
+    public static bool operator !=(Rational r1, Rational r2) => !(r1 == r2);
 
 
 
@@ -192,10 +291,7 @@ public class Rational
     /// <param name="r1">первый рациональное число</param>
     /// <param name="r2">второй рациональное число</param>
     /// <returns>возвращает true, если первый больше чем второй, false, если первый меньше чем второй</returns>
-    public static bool operator >(Rational r1, Rational r2)
-    {
-        return Rational.Fraction(r1) > Rational.Fraction(r2);
-    }
+    public static bool operator >(Rational r1, Rational r2) => Rational.Fraction(r1) > Rational.Fraction(r2);
 
 
 
@@ -260,8 +356,43 @@ public class Rational
         count_r1 = nok / r1._denominator;
         count_r2 = nok / r2._denominator;
         r1._numerator *= count_r1;
+        r1._denominator *= count_r1;
         r2._numerator *= count_r2;
+        r2._denominator *= count_r2;
         R1 = r1;
         R2 = r2;
+    }
+
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (obj.GetType() != typeof(Rational))
+        {
+            return false;
+        }
+        Rational r = (Rational)obj;
+        return Rational.Fraction(this) == Rational.Fraction(r);
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = 397;
+
+        unchecked
+        {
+            hash = (hash*0x18d)^_numerator.GetHashCode();
+            hash = (hash * 0x18d) ^ _numerator.GetHashCode();
+        }
+
+        return hash;
+    }
+
+    public override string ToString()
+    {
+        return $"Числитель:{_numerator}, Знаменатель:{_denominator}, Десятичный дробь:{Fraction(new Rational(_numerator, _denominator))}";
     }
 }
